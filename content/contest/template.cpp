2634c0a87f12521d5672fe1c0f316bdf01069c5c
Ruhan Habib <ruhanhabib39@gmail.com>
2023-10-15 14:09:09 +0600

    test 1

--- content/contest/template.cpp
+++ content/contest/template.cpp
@@ -9,6 +9,7 @@ typedef pair<int, int> pii;
 typedef vector<int> vi;
 
 int main() {
-	cin.tie(0)->sync_with_stdio(0);
-	cin.exceptions(cin.failbit);
+	ios_base::sync_with_stdio(false); cin.tie(0); cout.tie(0);
+
+	
 }
